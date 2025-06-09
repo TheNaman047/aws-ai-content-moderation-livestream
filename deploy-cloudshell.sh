ead9ff55f2d11e2dca9029bbc672e6547238060d
TheNaman047 <namankhandelwal314@gmail.com>
2025-06-09 15:46:12 +0530

    chore: updated script to avoid local dev setup

--- deploy-cloudshell.sh
+++ deploy-cloudshell.sh
@@ -1,16 +1,16 @@
 # Install Node CDK package
-sudo npm install -g aws-cdk
+# sudo npm install -g aws-cdk
 
 # Create Python Virtual Environment
-python3 -m venv .venv
-source .venv/bin/activate
+# python3 -m venv .venv
+# source .venv/bin/activate
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # install docker - to build the Lambda docker imageI have limited knowledge of Doc verification. But I think it also missed the piece integrating with the government database. To built a solution, we cannot only do AI.
-sudo yum update -y
-sudo amazon-linux-extras install -y docker
-sudo service docker start
+# sudo yum update -y
+# sudo amazon-linux-extras install -y docker
+# sudo service docker start
 
 # Bootstrap CDK - this step will launch a CloudFormation stack to provision the CDK package, which will take ~2 minutes.
 cdk bootstrap aws://${CDK_DEFAULT_ACCOUNT}/${CDK_DEFAULT_REGION}
